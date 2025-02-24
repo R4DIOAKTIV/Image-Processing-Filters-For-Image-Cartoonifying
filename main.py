ef60f3fc9b293094995f8447ee0ff619743b8f3a
R4DIOAKTIV <hattanyossri@yahoo.com>
2025-02-24 18:07:40 +0200

    main.py with needed functions names

---
+++ main.py
@@ -0,0 +1,16 @@
+import cv2 as cv
+import numpy as np
+
+
+def bgr2gray():
+
+def median_filter():
+    """smooth the image using a median filter"""
+def laplacian_filter():
+    """edge detection using a laplacian filter"""
+def threshold():
+    """threshold the image to get a binary image"""
+def bilateral_filter():
+    """small bilateral filter that goes over the image n times"""
+def overlay():
+    """overlay the bilaterally filtered image with the threshholded image"""
